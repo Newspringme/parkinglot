be62ae2e1e000d568993bdab4fd6692f66429b7e
87074004@qq.com <87074004@qq.com>
2020-06-04 20:00:40 +0800

    Initial commit

---
+++ src/test/java/com/cnzk/parkinglot/ParkinglotApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cnzk.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
