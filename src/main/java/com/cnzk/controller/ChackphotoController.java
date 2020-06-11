97ee29d82aef783d4a7cd648ac0683e923ac2ca5
liu <593621816@qq.com>
2020-06-11 10:19:35 +0800

    刘瀚元：出库 完整版

--- src/main/java/com/cnzk/controller/ChackphotoController.java
+++ src/main/java/com/cnzk/controller/ChackphotoController.java
@@ -51,4 +51,40 @@ public class ChackphotoController
 
 	}
 
+	@RequestMapping("/File2")
+	@ResponseBody
+	public String file2(@RequestParam("file2")MultipartFile file2){
+
+		String carnum = chackphotoService.file2(file2);
+		System.out.println("车牌-----------:"+carnum);
+
+		SimpleDateFormat sdf =   new SimpleDateFormat( " yyyy-MM-dd HH:mm:ss " );
+		String exittime = sdf.format(new Date());//查车找人
+
+		String username=chackphotoService.finduser(carnum);
+		System.out.println("入场用户-----------"+username);
+		//出场插入数据
+		chackphotoService.caraddexit(carnum,exittime);
+		//车辆情况查询 是临时还是有身份
+		String state;
+		if ("临时用户".equals(username)){
+			state = "临时车";
+		} else {
+			state=chackphotoService.findcarvip(carnum);
+		}
+		System.out.println("车辆情况---------"+state);
+		//查询入场时间
+		String entertime=chackphotoService.findentertime(carnum);
+		System.out.println("查询入场时间"+entertime);
+		//车位查询
+		String ps=null;
+		String time=null;
+		String money=null;
+		String date= carnum+","+username+","+state+","+ps+","+entertime+","+exittime+","+time+","+money;
+		System.out.println("车牌："+carnum+"用户名："+username+"车状态："+state+"车位："+ps+"进场时间："+entertime+"出场时间："+exittime+"总时长："+time+"收费："+money);
+		//		Object obj = new Gson().toJson()
+		return date;
+
+	}
+
 }
