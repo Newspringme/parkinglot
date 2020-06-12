e9b333a9ef692d8dfea05c5a9492a52fb6ac49b5
86189 <yu1529504122@qq.com>
2020-06-12 11:59:46 +0800

    余子翔_2020-6-12_提交

---
+++ src/main/java/com/cnzk/pojo/TbCoordinate.java
@@ -0,0 +1,58 @@
+package com.cnzk.pojo;
+
+
+public class TbCoordinate {
+
+    private Integer coordinateId;
+    private String coordinateName;
+    private String xyId;
+    private String coordinateX;
+    private String coordinateY;
+
+
+
+    public Integer getCoordinateId() {
+        return coordinateId;
+    }
+
+    public void setCoordinateId(Integer coordinateId) {
+        this.coordinateId = coordinateId;
+    }
+
+
+    public String getCoordinateName() {
+        return coordinateName;
+    }
+
+    public void setCoordinateName(String coordinateName) {
+        this.coordinateName = coordinateName;
+    }
+
+
+    public String getXyId() {
+        return xyId;
+    }
+
+    public void setXyId(String xyId) {
+        this.xyId = xyId;
+    }
+
+
+    public String getCoordinateX() {
+        return coordinateX;
+    }
+
+    public void setCoordinateX(String coordinateX) {
+        this.coordinateX = coordinateX;
+    }
+
+
+    public String getCoordinateY() {
+        return coordinateY;
+    }
+
+    public void setCoordinateY(String coordinateY) {
+        this.coordinateY = coordinateY;
+    }
+
+}
