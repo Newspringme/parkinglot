007c958f31ae58f7ddd20ee1c551e0e3afb8d31a
luochen <965813735@qq.com>
2020-06-15 10:50:01 +0800

    json修改

--- src/main/java/com/cnzk/service/FaceService.java
+++ src/main/java/com/cnzk/service/FaceService.java
@@ -1,18 +1,9 @@
 package com.cnzk.service;
 
-
 import com.cnzk.pojo.Admin;
 import com.cnzk.pojo.Charge;
 
-public interface FaceService
-{
-
-
-    //通过管理员账户,查询管理员信息
-    public Admin findAdminByAccount(String adminAccount);
-
-
-    //通过收费员账户,查询收费员信息
-    public Charge findChargeByAccount(String cashierAccount);
-
+public interface FaceService {
+    Admin findAdminByAccount(String adminnum);
+    Charge findChargeByAccount(String adminnum);
 }
