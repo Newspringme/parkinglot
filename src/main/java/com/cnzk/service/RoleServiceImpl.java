4e68911455a87fba7c20ea239f55ff21fdb2aacc
lq <675343019@qq.com>
2020-06-19 15:28:01 +0800

    权限管理更新，修改bug

--- src/main/java/com/cnzk/service/RoleServiceImpl.java
+++ src/main/java/com/cnzk/service/RoleServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cnzk.service;
 
+import com.alibaba.fastjson.JSON;
 import com.cnzk.mapper.AdminMapper;
 import com.cnzk.mapper.RoleMapper;
 import com.cnzk.pojo.*;
@@ -65,25 +66,43 @@ public class RoleServiceImpl implements RoleServeice{
     @Transactional
     public Integer updateMenuTree(String treeStr, Integer roleId) {
         Integer row = 0;
-        Gson gson = new Gson();
-        TreeData[] treeData = gson.fromJson(treeStr, TreeData[].class);
-        List<TbMenu> menutblList = new ArrayList<>();
-        for (int i=0;i<treeData.length;i++){
-            for (int j=0;j<treeData[i].getChildren().size();j++){
-                for (int z=0;z<treeData[i].getChildren().get(j).getChildren().size();z++){
-                    TbMenu tbMenu = new TbMenu();
-                    tbMenu.setMenuId(treeData[i].getChildren().get(j).getChildren().get(z).getId());
-                    tbMenu.setRoleId(roleId);
-                    tbMenu.setIsthreeMenu(0);
-                    menutblList.add(tbMenu);
-                }
-            }
-        }
-        System.out.println("------------");
+        List<TreeData> treeData = JSON.parseArray(treeStr,TreeData.class);
+        List<TbMenu> menutblList = new ArrayList<TbMenu>();
+        menutblList = digui(treeData,roleId,menutblList);
         System.out.println(menutblList.toString());
-        System.out.println("------------");
         row += roleMapper.deleRolesMenu(roleId);
         row += roleMapper.addRolesMenu(menutblList);
         return row;
+    }
+
+
+
+    public static void main(String[] args) {
+    String treeStr = "[{'id':100,'pid':0,'title':'管理员','spread':true,'checked':false,'children':[{'id':34,'pid':100,'title':'系统管理','icon':'fa fa-user','href':'','target':'','spread':true,'checked':false,'children':[{'id':18,'pid':34,'title':'角色管理','icon':'fa fa-street-view','href':'/parkinglot/pages/role-table.jsp','target':'_self','spread':true,'checked':false}]}]},{'id':101,'pid':0,'title':'收费员','target':'','spread':true,'checked':false,'children':[{'id':6,'pid':101,'title':'个人信息','icon':'fa fa-usd','href':'','spread':true,'checked':false,'children':[{'id':37,'pid':6,'title':'人脸录入','icon':'fa fa-user','href':'/parkinglot/pages/addChargeFace.jsp','target':'_blank','spread':true,'checked':true}]},{'id':35,'pid':101,'title':'月缴办理','icon':'fa fa-user','href':'/parkinglot/pages/combo_handle.jsp','target':'','spread':true,'checked':false,'children':[{'id':22,'pid':35,'title':'月卡办理','icon':'fa fa-flag-o','target':'_self','spread':true,'checked':true},{'id':23,'pid':35,'title':'月卡续费','icon':'fa fa-flag-checkered','target':'_self','spread':true,'checked':true}]},{'id':39,'pid':101,'title':'停车场信息','icon':'fa fa-tags','spread':true,'checked':false,'children':[{'id':24,'pid':39,'title':'查看停车场','icon':'fa fa-arrows','href':'/parkinglot/pages/parkinglot_map.jsp','target':'_self','spread':true,'checked':true},{'id':25,'pid':39,'title':'历史出场记录','icon':'fa fa-table','href':'/parkinglot/pages/exit_history.jsp','target':'_self','spread':true,'checked':true},{'id':32,'pid':39,'title':'实时出库','icon':'fa fa-car','href':'/parkinglot/pages/realtime_exit.jsp','target':'_self','spread':true,'checked':true},{'id':38,'pid':39,'title':'场内车辆信息','icon':'fa fa-car','target':'_self','spread':true,'checked':true}]}]}]";
+        Integer roleId = 3;
+        List<TreeData> treeData = JSON.parseArray(treeStr,TreeData.class);
+        List<TbMenu> menutblList = new ArrayList<TbMenu>();
+        menutblList = digui(treeData,roleId,menutblList);
+        System.out.println(menutblList.toString());
+    }
+
+
+    public static List<TbMenu> digui(List<TreeData> treeData,Integer roleId,List<TbMenu> menutblList){
+        System.out.println(treeData.size());
+        for (int i=0;i<treeData.size();i++){
+            TbMenu tbMenu = new TbMenu();
+            tbMenu.setMenuId(treeData.get(i).getId());
+            tbMenu.setRoleId(roleId);
+            tbMenu.setMenuName(treeData.get(i).getTitle());
+            System.out.println(tbMenu);
+            if (treeData.get(i).getChildren()!=null){
+                digui(treeData.get(i).getChildren(),roleId,menutblList);
+                tbMenu.setIsthreeMenu(1);
+            }else{
+                tbMenu.setIsthreeMenu(0);
+            }
+            menutblList.add(tbMenu);
+        }
+        return menutblList;
     }
 }
